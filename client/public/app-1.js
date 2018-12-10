ebce394203a56362012d3072dc46b7929752c38c
Zach Wiest <zachwiest@Zachs-MBP-3.fios-router.home>
2018-12-10 13:21:11 -0500

    I added app.js files as well as deleted other files

---
+++ client/public/app-1.js
@@ -0,0 +1,9 @@
+class ProductList extends RTCIceCandidate.Component {
+    render() {
+        return {
+            <div className='ui unstackable items'>
+            Hello, friend! I am a basic React component.
+            </div>
+        );
+    }
+}
